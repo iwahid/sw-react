0851228e18eec9ae9ee03094dd10aedcd582f9f3
olegzakirov <oleg.zakirov@saritasa.com>
2021-01-21 12:06:12 +0500

    JC19-100 implement html form

---
+++ script.js
@@ -0,0 +1,38 @@
+/*  event interception */
+let form = document.getElementById('form')
+form.addEventListener("submit", function formHandler(e) {
+  e.preventDefault()
+  postFormData()
+})
+
+/* collection data from HTML form */
+function dataCollector() {
+  const inputsCollection = [...document.getElementsByClassName('content')]
+
+  let dataList = {
+    fields: {},
+    model: 'resourse.people'
+  }
+
+  inputsCollection.forEach((input) => {
+    dataList.fields[input.id] = input.value
+  })
+
+  return dataList
+}
+
+async function postFormData() {
+
+  async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    return await response.json()
+  }
+
+  postData('https://js-camp-star-wars-default-rtdb.firebaseio.com/swapi/people.json', await dataCollector())
+    .then((data) => {
+      console.log("Data posted!", data)
+    })
+}
